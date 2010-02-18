94720587f2a8bf9c92e5c31a3ec219147c909296
Christoph Gritschenberger <e0525747@student.tuwien.ac.at>
2010-02-18 15:46:24 +0100

    disable logging in drools for now

--- core/workflow/service-engine/src/main/java/org/openengsb/drools/DroolsEndpoint.java
+++ core/workflow/service-engine/src/main/java/org/openengsb/drools/DroolsEndpoint.java
@@ -13,7 +13,7 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-   
+
  */
 package org.openengsb.drools;
 
@@ -85,7 +85,7 @@ public class DroolsEndpoint extends SimpleEventEndpoint {
         QName loggingServiceIdentification = getLoggingServiceIdentification();
         loggingMessageExchange.setService(loggingServiceIdentification);
         loggingMessageExchange.setInMessage(messageToLog);
-        send(loggingMessageExchange);
+        //send(loggingMessageExchange);
     }
 
     private QName getLoggingServiceIdentification() {
@@ -120,7 +120,7 @@ public class DroolsEndpoint extends SimpleEventEndpoint {
 
     /**
      * handle the MessageExchange with drools.
-     * 
+     *
      * @param e2 exchange to handle
      */
     protected void drools(Event e, MessageProperties msgProperties) {
